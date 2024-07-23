a075387fb9fce573822f10f9567738e4b13a1962
aryan22csu031 <aryan22csu031@ncuindia.edu>
2024-07-23 15:43:56 +0530

    added edit button

--- index.js
+++ index.js
@@ -23,4 +23,15 @@ app.get('/file/:filename', (req,res) => {
     fs.readFile(`./files/${req.params.filename}`, "utf-8", (err,filedata) => {
         res.render('show', {filename : req.params.filename, filedata : filedata})
     })
-})
+});
+
+app.get('/edit/:filename', (req,res) => {
+    res.render('edit', {filename : req.params.filename});
+});
+
+app.post('/edit', (req,res) => {
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, (err) => {
+        if(err) console.log(err);
+        res.redirect("/");
+    })
+});
